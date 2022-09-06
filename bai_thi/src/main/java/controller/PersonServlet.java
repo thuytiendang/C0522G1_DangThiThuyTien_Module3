b53f4493199f7b7b2c4a1e5e35663886dfd27a81
thuytiendang <thuytiendang2003@gmail.com>
2022-09-06 17:00:12 +0700

    m

--- bai_thi/src/main/java/controller/PersonServlet.java
+++ bai_thi/src/main/java/controller/PersonServlet.java
@@ -7,17 +7,49 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "PersonServlet", value = "/PersonServlet")
+@WebServlet(name = "PersonServlet", urlPatterns ={"", "/PersonServlet"})
 public class PersonServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        PersonRepository personRepository = new PersonRepository();
-        personRepository.showList();
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+
+                break;
+            case "update":
+
+                break;
+            case "delete":
+
+                break;
+            case "search":
+
+                break;
+            default:
+
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+
+                break;
+            case "update":
+
+                break;
+
+            default:
+        }
     }
 }
